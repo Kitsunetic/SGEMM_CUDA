2f02e2a0fcac19c1473d8818a9f13ad152e8ec65
Kitsunetic <jh.shim.gg@gmail.com>
2024-05-04 13:22:08 +0900

    update kernel 1 (naive), 2 (gmem coalesce)

--- src/kernels/2_kernel_global_mem_coalesce.cuh
+++ src/kernels/2_kernel_global_mem_coalesce.cuh
@@ -27,19 +27,21 @@
 // }
 
 template <const uint BLOCKSIZE>
-__global__ void sgemm_global_mem_coalesce(int M, int N, int K, float alpha,
-                                          const float *__restrict__ A, const float *__restrict__ B,
-                                          float beta, float *C)
+__global__ void sgemm_global_mem_coalesce(
+    const int M, const int N, const int K,
+    const float alpha,
+    const float *A,
+    const float *B,
+    const float beta,
+    float *C)
 {
-  const int c_row = blockIdx.x * BLOCKSIZE + (threadIdx.x / BLOCKSIZE);
-  const int c_col = blockIdx.y * BLOCKSIZE + (threadIdx.x % BLOCKSIZE);
+  const uint c_row = blockIdx.x * BLOCKSIZE + (threadIdx.x / BLOCKSIZE);
+  const uint c_col = blockIdx.y * BLOCKSIZE + (threadIdx.x % BLOCKSIZE);
   if (c_row >= M || c_col >= N)
     return;
 
   float tmp = 0.0;
   for (int i = 0; i < K; i++)
-  {
     tmp += A[c_row * K + i] * B[i * N + c_col];
-  }
   C[c_row * N + c_col] = alpha * tmp + beta * C[c_row * N + c_col];
 }

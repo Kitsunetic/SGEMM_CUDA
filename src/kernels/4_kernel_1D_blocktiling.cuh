9c1a750541b4a6dc84471671e5a81a45e8d30fc8
Kitsunetic <jh.shim.gg@gmail.com>
2024-05-05 08:59:34 +0900

    refac kernel 4

--- src/kernels/4_kernel_1D_blocktiling.cuh
+++ src/kernels/4_kernel_1D_blocktiling.cuh
@@ -10,9 +10,10 @@
 #define CEIL_DIV(M, N) (((M) + (N)-1) / (N))
 
 template <const int BM, const int BN, const int BK, const int TM>
-__global__ void sgemm1DBlocktiling(int M, int N, int K, float alpha,
-                                   const float *A, const float *B, float beta,
-                                   float *C) {
+__global__ void sgemm1DBlocktilingOriginal(int M, int N, int K, float alpha,
+                                           const float *A, const float *B, float beta,
+                                           float *C)
+{
   // If we flip x and y here we get ~30% less performance for large matrices.
   // The current, 30% faster configuration ensures that blocks with sequential
   // blockIDs access columns of B sequentially, while sharing the same row of A.
@@ -48,7 +49,8 @@ __global__ void sgemm1DBlocktiling(int M, int N, int K, float alpha,
   float threadResults[TM] = {0.0};
 
   // outer loop over block tiles
-  for (uint bkIdx = 0; bkIdx < K; bkIdx += BK) {
+  for (uint bkIdx = 0; bkIdx < K; bkIdx += BK)
+  {
     // populate the SMEM caches
     As[innerRowA * BK + innerColA] = A[innerRowA * K + innerColA];
     Bs[innerRowB * BN + innerColB] = B[innerRowB * N + innerColB];
@@ -59,11 +61,13 @@ __global__ void sgemm1DBlocktiling(int M, int N, int K, float alpha,
     B += BK * N;
 
     // calculate per-thread results
-    for (uint dotIdx = 0; dotIdx < BK; ++dotIdx) {
+    for (uint dotIdx = 0; dotIdx < BK; ++dotIdx)
+    {
       // we make the dotproduct loop the outside loop, which facilitates
       // reuse of the Bs entry, which we can cache in a tmp var.
       float tmpB = Bs[dotIdx * BN + threadCol];
-      for (uint resIdx = 0; resIdx < TM; ++resIdx) {
+      for (uint resIdx = 0; resIdx < TM; ++resIdx)
+      {
         threadResults[resIdx] +=
             As[(threadRow * TM + resIdx) * BK + dotIdx] * tmpB;
       }
@@ -72,9 +76,65 @@ __global__ void sgemm1DBlocktiling(int M, int N, int K, float alpha,
   }
 
   // write out the results
-  for (uint resIdx = 0; resIdx < TM; ++resIdx) {
+  for (uint resIdx = 0; resIdx < TM; ++resIdx)
+  {
     C[(threadRow * TM + resIdx) * N + threadCol] =
         alpha * threadResults[resIdx] +
         beta * C[(threadRow * TM + resIdx) * N + threadCol];
+  }
+}
+
+template <const int BM, const int BN, const int BK, const int TM>
+__global__ void sgemm1DBlocktiling(
+    const int M, const int N, const int K,
+    const float alpha,
+    const float *A,
+    const float *B,
+    const float beta,
+    float *C)
+{
+  __shared__ float As[BM * BK];
+  __shared__ float Bs[BK * BN];
+
+  const uint c_col = blockIdx.x;
+  const uint c_row = blockIdx.y;
+  const uint thread_col = threadIdx.x % BN;
+  const uint thread_row = threadIdx.x / BN;
+
+  A += c_row * BM * K;
+  B += c_col * BN;
+  C += c_row * BM * N + c_col * BN;
+
+  assert(BM * BK == blockDim.x);
+  assert(BN * BK == blockDim.x);
+  const uint inner_col_a = threadIdx.x % BK;
+  const uint inner_row_a = threadIdx.x / BK;
+  const uint inner_col_b = threadIdx.x % BN;
+  const uint inner_row_b = threadIdx.x / BN;
+
+  float thread_results[TM] = {0.0};
+  for (uint bk_idx = 0; bk_idx < K; bk_idx += BK)
+  {
+    As[inner_row_a * BK + inner_col_a] = A[inner_row_a * K + inner_col_a];
+    Bs[inner_row_b * BN + inner_col_b] = B[inner_row_b * N + inner_col_b];
+    __syncthreads();
+
+    for (uint dot_idx = 0; dot_idx < BK; dot_idx++)
+    {
+      float tmpB = Bs[dot_idx * BN + thread_col];
+      for (uint res_idx = 0; res_idx < TM; res_idx++)
+        thread_results[res_idx] += As[(thread_row * TM + res_idx) * BK + dot_idx] * tmpB;
+    }
+    __syncthreads();
+
+    A += BK;
+    B += BK * N;
+  }
+
+  for (uint res_idx = 0; res_idx < TM; res_idx++)
+  {
+    C[(thread_row * TM + res_idx) * N + thread_col] =
+        alpha * thread_results[res_idx] +
+        beta * C[(thread_row * TM + res_idx) * N + thread_col];
   }
 }
